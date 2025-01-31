d6c2a89b61ce8a41c7b4b370a741ad881dfd8341
Abdullahi Abdulkadir <graphgeo02@gmail.com>
2025-01-31 06:29:39 +0100

    set time interval

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Function to update the current time in UTC
 function updateCurrentTimeUTC() {
-    const currentTime = new Date().toISOString();
-    document.getElementById('current-time').textContent = `Current UTC Time: ${currentTime}`;
-  }
-  
-  // Update the time when the page loads
-  updateCurrentTimeUTC();
-  
+  const currentTime = new Date().toISOString();
+  document.getElementById('current-time').textContent = `Current UTC Time: ${currentTime}`;
+}
+
+// Update the time when the page loads
+updateCurrentTimeUTC();
+// update the time every second
+setInterval(updateCurrentTimeUTC, 10000);
+
